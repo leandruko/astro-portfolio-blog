b2fa17895006cfa95c367da17ad4273b54d6f918
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2024-10-23 21:18:44 -0300

    update

--- portafolio-blog/tailwind.config.js
+++ portafolio-blog/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}', // Escanea los archivos de tu proyecto
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  darkMode: ['class', '[data-theme="dark"]'], // Dark mode por clase o atributo
+};
