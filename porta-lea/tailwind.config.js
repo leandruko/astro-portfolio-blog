bbc7fbc295123193df49af54ed3ebcb53ae4a29a
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2024-11-04 16:46:29 -0300

    update

--- porta-lea/tailwind.config.js
+++ porta-lea/tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 6s ease infinite alternate',
+      },
       colors:{
         'negro':'#181C14',
         'verde':'#697565',
