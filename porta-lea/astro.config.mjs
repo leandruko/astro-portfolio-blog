ec42007715a33d2a78f5179a9b32d1936823c837
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2025-03-02 13:08:21 -0300

    update

--- porta-lea/astro.config.mjs
+++ porta-lea/astro.config.mjs
@@ -1,11 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
- 
-export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true, // set to false when using @vercel/analytics@1.4.0
-    },
-  }),
-});
+
+// https://astro.build/config
+export default defineConfig({});
