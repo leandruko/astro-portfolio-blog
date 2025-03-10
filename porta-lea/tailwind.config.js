88317bf7f8744494f5949aaf66208acc9bc108b4
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2025-03-10 13:57:15 -0300

    update background

--- porta-lea/tailwind.config.js
+++ porta-lea/tailwind.config.js
@@ -4,6 +4,28 @@ module.exports = {
     './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
   ],
   theme: {
+    patterns:{
+      opacities:{
+        100: "1",
+        80: ".80",
+        60: ".60",
+        40: ".40",
+        20: ".20",
+        10: ".10",
+        5: ".05",
+      },
+      sizes:{
+        1: "0.25rem",
+        2: "0.5rem",
+        4: "1rem",
+        6: "1.5rem",
+        8: "2rem",
+        16: "4rem",
+        20: "5rem",
+        24: "6rem",
+        32: "8rem",
+      }
+    },
     extend: {
       keyframes: {
         animatedgradient: {
@@ -37,5 +59,8 @@ module.exports = {
     'lg': '1440px',
     // => @media (min-width: 1440px) { ... }
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-bg-patterns'),
+  ],
 }
+
