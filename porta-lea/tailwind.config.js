50b5779c1c966fd4ba25e56841decfa69512f508
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2025-04-05 10:57:11 -0300

    update animations

--- porta-lea/tailwind.config.js
+++ porta-lea/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
   ],
+  plugins:[
+     require('tailwindcss-animated')
+  ],
   theme: {
     patterns:{
       opacities:{
