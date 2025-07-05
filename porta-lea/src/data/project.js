c8e3c4e96db78677afdb9dae5b8d6123e349442c
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2025-07-05 02:10:52 -0400

    update

--- porta-lea/src/data/project.js
+++ porta-lea/src/data/project.js
@@ -1,31 +1,28 @@
 // src/data/projects.js
 export const projects = [
   {
-    id: 'analisis-ventas-retail',
-    title: 'Análisis de Ventas para Retail',
-    description: 'Construcción de un dashboard interactivo para analizar tendencias de ventas, rendimiento de productos y comportamiento del cliente en una cadena de tiendas retail.',
-    image: '/images/projects/ventas-retail-thumb.webp', // Asegúrate de tener una imagen aquí
-    technologies: ['Python', 'Pandas', 'SQL', 'Tableau'],
-    githubUrl: 'https://github.com/tu-usuario/analisis-ventas-retail',
-    demoUrl: 'https://tu-demo.com/dashboard-ventas-retail', // Opcional
+    id: 'analisis-sismos-chile',
+    title: 'Análisis de sismos en Chile 2000-2024',
+    description: 'En este proyecto se realiza un análisis exhaustivo de los sismos en Chile entre los años 2000 y 2024, con el objetivo de identificar patrones, realizar inferencias y presentar conclusiones basadas en la información disponible. Se exploran y visualizan los datos para entender las características y tendencias.',
+    image: '/images/sismo-chile.webp',
+    technologies: ['Python', 'Pandas','Jupyter'],
+    githubUrl: 'https://github.com/leandruko/sismos-Chile'
   },
   {
-    id: 'pipeline-etl-ecommerce',
-    title: 'Pipeline ETL para E-commerce',
-    description: 'Diseño e implementación de un pipeline ETL robusto para ingestar y transformar datos de ventas, inventario y usuarios de una plataforma de e-commerce a un data warehouse.',
-    image: '/images/projects/etl-ecommerce-thumb.webp',
-    technologies: ['Python', 'Apache Airflow', 'PostgreSQL', 'AWS S3', 'Glue'],
-    githubUrl: 'https://github.com/tu-usuario/pipeline-etl-ecommerce',
-    // demoUrl: '', // No aplica si es solo backend
+    id: 'morosidad-sistema-financiero',
+    title: 'Morosidad sistema financiero',
+    description: 'En este proyecto se realiza un análisis de datos y aplicar modelos de machine learning para predecir si un usuario será deudor en el sistema financiero. A través del análisis exploratorio de datos y la implementación de algoritmos de clasificación, buscamos encontrar patrones que nos permitan hacer predicciones precisas sobre la morosidad de los clientes.',
+    image: '/images/morosidad.jpeg',
+    technologies: ['Python', 'Pandas','Seaborn','Mathplotlib','Scikit-learn','Jupyter'],
+    githubUrl: 'https://github.com/leandruko/Morosidad-Sistema-Financiero',
   },
   {
-    id: 'modelado-riesgo-credito',
-    title: 'Modelado de Riesgo de Crédito',
-    description: 'Desarrollo de un modelo de machine learning para predecir el riesgo de impago en solicitudes de crédito, mejorando la precisión de las decisiones financieras.',
-    image: '/images/projects/riesgo-credito-thumb.webp',
-    technologies: ['Python', 'Scikit-learn', 'SQL', 'Jupyter', 'Streamlit'],
-    githubUrl: 'https://github.com/tu-usuario/modelado-riesgo-credito',
-    demoUrl: 'https://tu-demo.com/riesgo-credito-app', // Opcional
+    id: 'cancer-pulmon',
+    title: 'Cáncer de pulmón',
+    description: 'Proyecto realizado con el objetivo de predecir si una persona tiene cáncer al pulmón, basándonos en el dataset lung cáncer de Kaggle, aplicando análisis de datos, limpieza y modelos de machine learning',
+    image: '/images/cancer.webp',
+    technologies: ['Python', 'Scikit-learn', 'Jupyter'],
+    githubUrl: 'https://github.com/leandruko/lung-cancer'
   },
   // Añade más proyectos aquí siguiendo la misma estructura
 ];
