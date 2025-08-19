db9651c370f42646ce99875531573b0166bf60ea
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2025-08-19 17:59:09 -0400

    update

--- porta-lea/src/data/project.js
+++ porta-lea/src/data/project.js
@@ -5,7 +5,7 @@ export const projects = [
     title: 'Análisis de sismos en Chile 2000-2024',
     description: 'En este proyecto se realiza un análisis exhaustivo de los sismos en Chile entre los años 2000 y 2024, con el objetivo de identificar patrones, realizar inferencias y presentar conclusiones basadas en la información disponible. Se exploran y visualizan los datos para entender las características y tendencias.',
     image: '/images/sismo-chile.webp',
-    technologies: ['Python', 'Pandas','Jupyter'],
+    technologies: ['Python', 'Pandas','Seaborn','Mathplotlib','Scikit-learn'],
     githubUrl: 'https://github.com/leandruko/sismos-Chile'
   },
   {
@@ -21,7 +21,7 @@ export const projects = [
     title: 'Cáncer de pulmón',
     description: 'Proyecto realizado con el objetivo de predecir si una persona tiene cáncer al pulmón, basándonos en el dataset lung cáncer de Kaggle, aplicando análisis de datos, limpieza y modelos de machine learning',
     image: '/images/cancer.webp',
-    technologies: ['Python', 'Scikit-learn', 'Jupyter'],
+    technologies: ['Python', 'Pandas','Seaborn','Mathplotlib','Scikit-learn'],
     githubUrl: 'https://github.com/leandruko/lung-cancer'
   },
   // Añade más proyectos aquí siguiendo la misma estructura
