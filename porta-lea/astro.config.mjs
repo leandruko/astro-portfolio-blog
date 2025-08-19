5fb17bbbdd78ad409e8afca415b50e2511227937
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2025-08-19 16:23:24 -0400

    update

--- porta-lea/astro.config.mjs
+++ porta-lea/astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   output: 'server',
+
   adapter: vercel({
     webAnalytics: {
       enabled: true, // set to false when using @vercel/analytics@1.4.0
     },
   }),
+
+  integrations: [react()],
 });
