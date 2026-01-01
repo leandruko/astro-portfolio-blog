ba7d7a31f3071cf3842040e99aa8b26f29e6ccb4
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2026-01-01 18:12:40 -0300

    update

--- porta-lea/src/data/project.js
+++ porta-lea/src/data/project.js
@@ -1,36 +1,110 @@
 // src/data/projects.js
+
 export const projects = [
-  {
-    id: 'analisis-sismos-chile',
-    title: 'Análisis de sismos en Chile 2000-2024',
-    description: 'En este proyecto se realiza un análisis exhaustivo de los sismos en Chile entre los años 2000 y 2024, con el objetivo de identificar patrones, realizar inferencias y presentar conclusiones basadas en la información disponible. Se exploran y visualizan los datos para entender las características y tendencias.',
-    image: '/images/sismo-chile.webp',
-    technologies: ['Python', 'Pandas','Seaborn','Mathplotlib','Scikit-learn'],
-    githubUrl: 'https://github.com/leandruko/sismos-Chile'
+    {
+    id: "Davton",
+
+    title: "Davton DJ",
+
+    description:
+      "Landing page desarrollada para DJ Davton.",
+
+    image: "/images/davton.png",
+
+    technologies: ["Astro", "Tailwind", "CSS"],
+
+    demoUrl: "https://www.davton.cl/",
   },
-  {
-    id: 'morosidad-sistema-financiero',
-    title: 'Morosidad sistema financiero',
-    description: 'En este proyecto se realiza un análisis de datos y aplicar modelos de machine learning para predecir si un usuario será deudor en el sistema financiero. A través del análisis exploratorio de datos y la implementación de algoritmos de clasificación, buscamos encontrar patrones que nos permitan hacer predicciones precisas sobre la morosidad de los clientes.',
-    image: '/images/morosidad.jpeg',
-    technologies: ['Python', 'Pandas','Seaborn','Mathplotlib','Scikit-learn','Jupyter'],
-    githubUrl: 'https://github.com/leandruko/Morosidad-Sistema-Financiero',
+
+    {
+    id: "cimasur",
+
+    title: "Sistema de trazabilidad Cimasur",
+
+    description:
+      "Sistema creado con el fin de registrar inforamcion de logista en la empresa.",
+
+    image: "/images/cimasur.png",
+
+    technologies: ["DJANGO","JS","PYTHON","POSTGRES"],
   },
-  {
-    id: 'cancer-pulmon',
-    title: 'Cáncer de pulmón',
-    description: 'Proyecto realizado con el objetivo de predecir si una persona tiene cáncer al pulmón, basándonos en el dataset lung cáncer de Kaggle, aplicando análisis de datos, limpieza y modelos de machine learning',
-    image: '/images/cancer.webp',
-    technologies: ['Python', 'Pandas','Seaborn','Mathplotlib','Scikit-learn'],
-    githubUrl: 'https://github.com/leandruko/lung-cancer'
+
+    {
+    id: "sushi-kenzo",
+
+    title: "Toti Kenzo Sushi",
+
+    description:
+      "Pagina creada con el finde mostrar productos.",
+
+    image: "/images/sushi.png",
+
+    technologies: ["Astro", "Tailwind", "CSS","DECAP CMS"],
+
+    demoUrl: "https://www.totikenzosushi.cl/",
   },
+  
   {
-    id: 'sushi-kenzo',
-    title: 'Toti Kenzo Sushi',
-    description: 'Proyecto realizado con el objetivo de predecir si una persona tiene cáncer al pulmón, basándonos en el dataset lung cáncer de Kaggle, aplicando análisis de datos, limpieza y modelos de machine learning',
-    image: '/images/sushi.png',
-    technologies: ['Astro', 'Tailwind','CSS'],
-    githubUrl: 'https://www.totikenzosushi.cl/',
-    demoUrl: 'https://www.totikenzosushi.cl/'
+    id: "analisis-sismos-chile",
+
+    title: "Análisis de sismos en Chile 2000-2024",
+
+    description:
+      "Proyecto se realiza un análisis de los sismos en Chile entre los años 2000 y 2024.",
+
+    image: "/images/sismo-chile.webp",
+
+    technologies: [
+      "Python",
+      "Pandas",
+      "Seaborn",
+      "Mathplotlib",
+      "Scikit-learn",
+    ],
+
+    githubUrl: "https://github.com/leandruko/sismos-Chile",
+  },
+
+  {
+    id: "morosidad-financiero",
+
+    title: "Morosidad sistema financiero",
+
+    description:
+      "Análisis de datos para predecir si alguien será deudor.",
+
+    image: "/images/morosidad.jpeg",
+
+    technologies: [
+      "Python",
+      "Pandas",
+      "Seaborn",
+      "Mathplotlib",
+      "Scikit-learn",
+      "Jupyter",
+    ],
+
+    githubUrl: "https://github.com/leandruko/Morosidad-Sistema-Financiero",
+  },
+
+  {
+    id: "cancer-pulmon",
+
+    title: "Cáncer de pulmón",
+
+    description:
+      "Desarrollado con el objetivo de predecir si una persona tiene cáncer al pulmón.",
+
+    image: "/images/cancer.webp",
+
+    technologies: [
+      "Python",
+      "Pandas",
+      "Seaborn",
+      "Mathplotlib",
+      "Scikit-learn",
+    ],
+
+    githubUrl: "https://github.com/leandruko/lung-cancer",
   },
 ];
