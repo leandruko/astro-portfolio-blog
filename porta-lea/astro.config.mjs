439ed3745b35eabd4b4cf4c094a11c557d1a649d
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2026-02-07 02:01:50 -0300

    update cloudflare

--- porta-lea/astro.config.mjs
+++ porta-lea/astro.config.mjs
@@ -3,14 +3,12 @@ import vercel from '@astrojs/vercel';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   output: 'server',
 
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true, // set to false when using @vercel/analytics@1.4.0
-    },
-  }),
+  adapter: cloudflare(),
 
   integrations: [react()],
 });
